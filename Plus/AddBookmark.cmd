448a2aee38055757165863aa5d3df302f4c54062
GitHubRulesOK <GitHubRulesOK@users.noreply.github.com>
2024-11-04 04:00:57 +0000

    Add files via upload

--- Plus/AddBookmark.cmd
+++ Plus/AddBookmark.cmd
@@ -1,10 +1,17 @@
-@Mode 60,17 & Color 9F & Title SumatraPDF addin Add Bookmark [via cpdf] v'24-11-04--01
+@Mode 60,17 & Color 9F & Title SumatraPDF addin Add Bookmark [via cpdf] v'24-11-04--02
 @echo off & SetLocal EnableDelayedExpansion & pushd %~dp0 & goto MAIN
 Do not delete the above two lines since they are needed to prepare this script.
 
 ToDo 
 Add more description about how to use ?
 
+This file is based on placed in a Plus folder where SumatraPDF-settings.txt is
+for example
+C:\Users\Your name\AppData\Local\SumatraPDF\SumatraPDF-settings.txt
+
+C:\Users\Your name\AppData\Local\SumatraPDF\Plus\AddBookmark.cmd
+
+Only on first run it needs internet access to download recent cpdf.exe
 
 :MAIN
 set /a "test=0+%2"
