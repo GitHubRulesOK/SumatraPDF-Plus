4d50c9ee3ebc6ae3791d0a2c3e11bf9deb435cc0
GitHubRulesOK <GitHubRulesOK@users.noreply.github.com>
2024-12-04 05:51:42 +0000

    Update List-Bookmarks.cmd

--- Plus/List-Bookmarks.cmd
+++ Plus/List-Bookmarks.cmd
@@ -1,4 +1,4 @@
-@Mode 60,17 & Color 9F & Title SumatraPDF addin Export Bookmarks [via cpdf] v'24-12-04--01
+@Mode 60,17 & Color 9F & Title SumatraPDF addin Export Bookmarks [via cpdf] v'24-12-04--02
 @echo off & SetLocal EnableDelayedExpansion & pushd %~dp0 & goto MAIN
 Do not delete the above two lines since they are needed to prepare this script.
 
@@ -9,7 +9,7 @@ This file is based on placed in a Plus folder where SumatraPDF-settings.txt is
 for example
 C:\Users\Your name\AppData\Local\SumatraPDF\SumatraPDF-settings.txt
 
-C:\Users\Your name\AppData\Local\SumatraPDF\Plus\AddBookmark.cmd
+C:\Users\Your name\AppData\Local\SumatraPDF\Plus\List-Bookmarks.cmd
 
 Only on first run it needs internet access to download recent cpdf.exe
 
@@ -20,7 +20,7 @@ set "cpdf=%~dp0cpdf\cpdf.exe"
 if not exist "%cpdf%" goto :dependencies
 
 rem export existing bookmarks to filename folder
-"%cpdf%" -list-bookmarks -utf8 "%~dpn1.pdf" 2>nul 1>"%~dpn1-pdf.bmk"
+"%cpdf%" -list-bookmarks -utf8 "%~dpn1.pdf" 2>nul 1>"%~dpn1-pdf.bkm"
 
 REM clean-up
 :eof
