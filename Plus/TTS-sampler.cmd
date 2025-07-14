4f7e877915e0bf08fdd6280a0e49280eb325866d
GitHubRulesOK <GitHubRulesOK@users.noreply.github.com>
2025-07-14 10:10:19 +0100

    Update TTS-sampler.cmd

--- Plus/TTS-sampler.cmd
+++ Plus/TTS-sampler.cmd
@@ -1,6 +1,9 @@
-@echo off&Mode 35,3&Color 3F&Title TTS-sampler v2025-13-07-02
+@echo off&Mode 35,3&Color 3F&Title TTS-sampler v 2025-07-14-03
 REM see IMPORTANT  notes below
 goto MAIN
+
+Version 3 has minor edits such as check filesize and corrected above date
+Version 2 had minor edits such as ensure default voice is set.
 
 This is not intended as a fully working application but a SAMPLE
 of what is possible by using a multi-format file to text conversion.
@@ -8,18 +11,19 @@ of what is possible by using a multi-format file to text conversion.
 Balabolka text extraction can work with many SumatraPDF file formats such as
  
 AZW, AZW3, CHM, DjVu, EPUB, FB2, FB3, HTML, MD, MHT, MOBI, PDF, PRC,
-TCR, TXT (aswell as others see https://www.cross-plus-a.com/btext.htm)
+TCR, TXT (as well as others see https://www.cross-plus-a.com/btext.htm)
 The IFilter interface may be used for files with unknown extensions.
 
-For OCR of images a tesseract engine would be required and filetyes filtered.
+For OCR of images a tesseract engine would be required and filetypes filtered.
 
-This file will download on 1st run the latest dependencies if not present.
+This file will download on 1st run the latest TTS dependencies if not present.
 
-Can be run standalone or via ExternalViewers in SumatraPDF-settings.txt. For example
+Can be run standalone or via ExternalViewers in SumatraPDF-settings.txt. For whole file.
+To use selected pages would need a different post processing approach.
 
 ExternalViewers [
 	[
-		CommandLine = "C:\Users\K\AppData\Local\SumatraPDF\plus\TTS-sampler.cmd" "%1" page=1-N
+		CommandLine = "C:\Users\K\AppData\Local\SumatraPDF\plus\TTS-sampler.cmd" "%1"
 		Name = Send file &To TTS sampler
 		Filter = *.*
 		Key = T
@@ -27,17 +31,19 @@ ExternalViewers [
 ]
 
 :MAIN
-REM set your choice of X2T and TTS.exe here and edit options in later lines
-REM IMPORTANT change the following lines for your own instalation
-
-REM IMPORTANT if you see the Balabolka message Error: OLE error 8004503A
-REM it usually means a voice was not found or assigned ! default here is -n Zira
+REM set your choice of X2T and TTS.exe actions here and edit options in later lines
+REM IMPORTANT change the following lines for your own instalation this aborts if file is considered too big
+for %%I in ("%~1") do (
+  if %%~zI GTR 10485760 echo Warning: File bigger than 10 MB.   Closing, consider slicing manually!&pause&exit/b
+)
 
 set "plus=%~dp0"
-
 set "X2T=%plus%blb2txt\blb2txt.exe"
 set "TTS=%plus%balcon\balcon.exe"
 if not exist "%X2T%" goto dependencies
+
+REM IMPORTANT if you see the Balabolka message Error: OLE error 8004503A
+REM it usually means a voice was not found or assigned ! default here is -n Zira
 
 REM delete any prior run
 if exist "%temp%\Balcon-TTS.txt" del "%temp%\Balcon-TTS.txt"
