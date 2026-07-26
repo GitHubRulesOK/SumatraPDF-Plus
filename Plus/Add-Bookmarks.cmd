4a18226a3263e891a52fb2277e4d44e1a38fd4ec
GitHubRulesOK <GitHubRulesOK@users.noreply.github.com>
2026-07-26 17:36:43 +0100

    Add files via upload

--- Plus/Add-Bookmarks.cmd
+++ Plus/Add-Bookmarks.cmd
@@ -1,6 +1,10 @@
-@Mode 60,17 & Color 9F & Title SumatraPDF Import Bookmarks [via cpdf] v'26-01-21--01
+@Mode 65,17 & Color 9F & Title SumatraPDF Import Bookmarks [via cpdf] v'26-07-26--02
 @echo off & SetLocal EnableDelayedExpansion & pushd %~dp0 & goto MAIN
 Do not delete the above two lines since they are needed to prepare this script.
+
+Updated from v'26-01-21--01 
+- changed filenames to match sumatrapdf-tool run List-BM.js which uses export and import filename-BM.txt
+- improved download link for cpdf.exe dependency
 
 ToDo 
 Add more description about how to use with set level before list ?
@@ -12,7 +16,7 @@ C:\Users\Your name\AppData\Local\SumatraPDF\SumatraPDF-settings.txt
 
 C:\Users\Your name\AppData\Local\SumatraPDF\Plus\Add-Bookmarks.cmd
 
-Only on first run it may need internet access to download recent cpdf.exe
+Only on first run it may need internet access to download most recent cpdf.exe
 
 To RUN either drag and drop a PDF file on this CMD or in SumatraPDF Advanced options add it to 
 
@@ -20,21 +24,22 @@ To RUN either drag and drop a PDF file on this CMD or in SumatraPDF Advanced opt
 ExternalViewers [
 	[
 		CommandLine = "C:\Users\ PUT your user name here \AppData\Local\SumatraPDF\plus\Add-Bookmarks.cmd" "%1" 
-		Name = Import all &Bookmarks for this PDF as filename.bkm
+		Name = Import all &Bookmarks for this PDF as filename-BM.txt
 		Filter = *.pdf
 		Key = +
 	]
 ]
 
 :MAIN
-if not exist "%~dpn1-pdf.bkm" goto help
+if not exist "%~dpn1-BM.txt" goto help
 
 set "cpdf=%~dp0cpdf\cpdf.exe"
 if not exist "%cpdf%" goto :dependencies
 
 rem import existing bookmarks from filename folder
-"%cpdf%" -add-bookmarks "%~dpn1-pdf.bkm" "%~dpn1.pdf" -o "%~dpn1-bookmarked.pdf"
-dir /b "%~dpn1-bookmarked.pdf" & pause
+"%cpdf%" -add-bookmarks "%~dpn1-BM.txt" "%~dpn1.pdf" -o "%~dpn1-BM.pdf"
+rem dir /b "%~dpn1-BM.pdf" & pause
+start SumatraPDF.exe -reuse-instance "%~dpn1-BM.pdf"
 REM clean-up
 :eof
 exit /b
@@ -42,10 +47,10 @@ exit /b
 :dependencies
 md cpdf
 cd cpdf
-curl -O https://raw.githubusercontent.com/coherentgraphics/cpdf-binaries/master/Windows32bit/cpdf.exe
+curl -LO https://github.com/coherentgraphics/cpdf-binaries/raw/refs/heads/master/Windows-32bit/cpdf.exe
 cd ..
 pause &exit /b
 
 :help
-echo needs input filename.pdf that has an adjacent filename-pdf.bkm
+echo needs input filename.pdf that has an adjacent filename-BM.txt
 pause
