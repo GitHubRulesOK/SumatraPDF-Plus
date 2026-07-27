7b5d2e0ae2bfcac99dd52b9117b98ac08ddf0196
GitHubRulesOK <GitHubRulesOK@users.noreply.github.com>
2026-07-27 15:53:02 +0100

    Update AddBookmark.cmd

--- Plus/AddBookmark.cmd
+++ Plus/AddBookmark.cmd
@@ -1,9 +1,37 @@
-@Mode 60,17 & Color 9F & Title SumatraPDF addin Add Bookmark [via cpdf]
-@echo off
-setlocal EnableDelayedExpansion
-pushd %~dp0
+@Mode 65,17 & Color 9F & Title SumatraPDF addin Add Bookmark [via cpdf]  v'26-07-26--02
+@echo off & SetLocal EnableDelayedExpansion & pushd %~dp0 & goto MAIN
+Do not delete the above two lines since they are needed to prepare this script.
+
+Updated from v'26-01-21--01 
+- improved export & import logic
+- improved download link for cpdf.exe dependency
+
+ToDo 
+Add more description about how to use with set level before list ?
+
+This file is based on placed in a Plus folder below where SumatraPDF-settings.txt is
+
+for example
+C:\Users\Your name\AppData\Local\SumatraPDF\SumatraPDF-settings.txt
+
+C:\Users\Your name\AppData\Local\SumatraPDF\Plus\Add-Bookmarks.cmd
+
+Only on first run it may need internet access to download most recent cpdf.exe
+
+To RUN either drag and drop a PDF file on this CMD or in SumatraPDF Advanced options add it to 
+
+
+ExternalViewers [
+	[
+		CommandLine = "C:\Users\ PUT your user name here \AppData\Local\SumatraPDF\plus\Add-Bookmarks.cmd" "%1" 
+		Name = Import all &Bookmarks for this PDF as filename-BM.txt
+		Filter = *.pdf
+		Key = +
+	]
+]
+
 :: =====
-::  MAIN
+:MAIN
 :: =====
 set /a "test=0+%2"
 if "!test!" == "" goto help
@@ -69,7 +97,7 @@ exit /b
 :: ===
 md cpdf
 cd cpdf
-curl -LO https://raw.githubusercontent.com/coherentgraphics/cpdf-binaries/master/Windows32bit/cpdf.exe
+curl -LO https://github.com/coherentgraphics/cpdf-binaries/raw/refs/heads/master/Windows-32bit/cpdf.exe
 cd ..
 pause
 exit /b
